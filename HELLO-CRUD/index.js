387955dbe5a26a04985c6f5b800eb43b683e07b6
Werter <werterpires23@hotmail.com>
2021-05-27 20:36:17 -0300

    tentando consertar erros

---
+++ HELLO-CRUD/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express()  ;
+app.use(express.json());
+
+const port = 30000;
+
+const usuarioWerter = {
+    id: 1,
+    nome: 'Werter',
+    idade: 29
+};
+
+const usuarios = [usuarioWerter]
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(port, () => {
+console.log(`Server is up and runnig on port ${port}`)
+});
