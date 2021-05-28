17cbd7bec98ead9674ff63610659924eb60e33ba
Werter <werterpires23@hotmail.com>
2021-05-27 21:58:24 -0300

    crud criado

--- HELLO-CRUD/index.js
+++ HELLO-CRUD/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()  ;
 app.use(express.json());
 
-const port = 30000;
+const port = 40000;
 
 const usuarioWerter = {
     id: 1,
@@ -18,6 +18,45 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
+app.post('/usuarios', (req, res) =>{
+    const {nome, idade} = req.body;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.json(novoUsuario);
+
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id} = req.params;
+    const { nome, idade } = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id));
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuario)
+})
+
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    
+    usuarios.splice(index, 1);
+    res.status(204).send();
+    });
+
+
+
 app.listen(port, () => {
 console.log(`Server is up and runnig on port ${port}`)
 });
+
